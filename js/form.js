ff73fd8e6dc4886a6da06d18cc1262d9ef1ce15f
d-masch <energodesign@yandex.ru>
2021-10-04 13:41:36 +0300

    ТЗ-15

--- js/form.js
+++ js/form.js
@@ -1,7 +1,9 @@
 const someObjectkey = document.getElementById("some-object-key");
 const someObjectvalue = document.getElementById("some-object-value");
+const someObject = {};
 const btn = document.getElementById("btn");
 btn.onclick = () => {
-  document.write("key is: " + someObjectkey.value);
-  document.write("   velue is: " + someObjectvalue.value);
-};
+  someObject.someObjectkey = someObjectvalue;
+  console.log(someObjectkey.value, someObjectvalue.value);
+ };
+console.log(someObject);
