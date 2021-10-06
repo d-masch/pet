0a9d1f2cf86e090562560951406e27c6944e99f5
d-masch <energodesign@yandex.ru>
2021-10-06 22:57:20 +0300

    Т3-15

---
+++ js/experaments.js
@@ -0,0 +1,6 @@
+let str = "";
+console.log(str);
+const str2 = str + "one";
+//str = str + "two";
+
+str += "two"
--- js/form.js
+++ js/form.js
@@ -1,11 +1,18 @@
 const keyInput = document.getElementById("some-object-key");
 const valueInput = document.getElementById("some-object-value");
-const someObject = {};
+const out = document.getElementById("Out");
 const btn = document.getElementById("btn");
+const someObject = {};
+function getOutput(obj) {
+  let output = "";
+  for (const key in obj) {
+    output += `
+ <div class="outKey">Key: ${key} 
+ Value: ${obj[key]}</div>`;
+  }
+  return output;
+}
 btn.onclick = () => {
   someObject[keyInput.value] = valueInput.value;
-  let out = document.getElementById("Out");
-  out.innerHTML += `
- <div class="outKey">Key: ${keyInput.value} 
- Value: ${someObject[keyInput.value]}</div>`;
+  out.innerHTML = getOutput(someObject);
 };
